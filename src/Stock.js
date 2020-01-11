dd8602b78cd81532a1fe7035974fa48b19349289
jon-wade <jonwadeuk@gmail.com>
2020-01-11 10:32:40 +0000

    update readme.md and add some additional code comments

--- src/Stock.js
+++ src/Stock.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { StockVisibilitySwitcher } from './StockVisibilitySwitcher'
 
+// this is only smart component in the repo, which handles state to enable stock to be hidden or shown
 export class Stock extends Component {
   constructor (props) {
     super(props)
@@ -22,12 +23,14 @@ export class Stock extends Component {
     if (stock.length > 0) {
       return stock.map((stk, i) => {
         const { product, qty, replenishment } = stk
+        // Each element of stock content is enclosed in a section tag as it represents a distinct content unit
         return (
           <section key={ i }>
             <StockVisibilitySwitcher
               checked={ checked }
               handleCheckboxChange={ this.handleCheckboxChange }
             />
+            { /* this is the rendering mechanism for showing or hiding stock content */ }
             { this.state.checked &&
             <ul>
               <li>Product: { product }</li>
